962f0167ca908690beedef100e1a1fcc61e233b3
leiper.mike <leiper123.mike@outlook.com>
2025-01-30 18:45:01 -0500

    only one maze

--- src/main.py
+++ src/main.py
@@ -4,14 +4,10 @@ from point import Point
 from cell import Cell
 from maze import Maze
 def main():
-     win = Window(1024, 920)
-     maze = Maze(5, 5, 10, 10, 10, 10, window=win)
-     maze.solve()
-     maze = Maze(200, 200, 10, 10, 10, 10, window=win)
-     maze.solve()
-     maze = Maze(5, 200, 10, 10, 10, 10, window=win)
-     maze.solve()
-     maze = Maze(200, 5, 10, 10, 10, 10, window=win)
+     win = Window(1024, 1024)
+     maze = Maze(5, 5, 25, 25, 10, 10, window=win)
      maze.solve()
      win.wait_for_close()
+
+     
 main()
